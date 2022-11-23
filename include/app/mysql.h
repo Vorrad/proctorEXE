b2a6734264e2c7d446045448821cda391cc87b04
Vorrad <15217340356@163.com>
2022-11-22 22:24:57 -0800

    feat: 新增创建和连接数据库功能

--- include/app/mysql.h
+++ include/app/mysql.h
@@ -1,7 +1,12 @@
 /*引入连接Mysql的头文件*/
 #include "/usr/include/mysql/mysql.h"
 
-// 定义MySQL服务器信息
-#define HOST "localhost" /*MySql服务器地址*/
-#define USERNAME "z" /*用户名*/
-#define PASSWORD "z" /*数据库连接密码*/
+// MySQL服务器信息
+// 可自定义此部分
+#define HOST "localhost"            /*MySql服务器地址*/
+#define USERNAME "root"             /*root用户名*/
+#define PASSWORD NULL               /*数据库连接密码，root用户默认不需要密码*/
+#define DATABASE "proctorEXE_db"    /*数据库名*/
+
+// SQL查询参数
+#define BUFFER_SIZE 4096            /*查询语句最大长度*/
--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@
 src_file=$(find ./src/app -name "*.c")
 inc_file=$(find ./include -name "*.h")
 
-gcc -o bin/start $src_file $inc_file
+gcc -o bin/start $src_file $inc_file -lmysqlclient
