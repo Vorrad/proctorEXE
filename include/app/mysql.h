e85b68d0ef7a4cdc3f66991a4d136408852f9f9d
Vorrad <15217340356@163.com>
2022-11-26 00:08:55 -0800

    refractor: 优化数据库判断逻辑

--- include/app/mysql.h
+++ include/app/mysql.h
@@ -12,20 +12,29 @@
 #define USERNAME "root"             /*root用户名*/
 #define PASSWORD NULL               /*数据库连接密码，root用户默认不需要密码*/
 #define DATABASE "proctorEXE_db"    /*数据库名*/
+#define TABLE_PROGRAM "program"     /*数据表*/
+#define TABLE_POLICY "policy"       /*数据表*/
+#define TABLE_AUDIT "audit"         /*数据表*/
 
 // 函数定义
 
 // 登录数据库
-void login_to_db(MYSQL* mysql, const char* host, const char* username, const char* password);
+int login_to_db(MYSQL* mysql, const char* host, const char* username, const char* password);
 
 // 连接数据库（全过程）
-void connect_to_db(MYSQL* mysql, const char* db_name, MYSQL_RES** result);
+int connect_to_db(MYSQL* mysql, const char* db_name);
 
 // 检查数据库是否已经存在
-bool db_exists(MYSQL* mysql, const char* db_name, MYSQL_RES** result);
+bool db_exists(MYSQL* mysql, const char* db_name);
 
 // 从名称创建数据库
 int create_db(MYSQL* mysql, const char* db_name);
 
 // 使用数据库
 int use_db(MYSQL* mysql, const char* db_name);
+
+// 检查表是否存在
+bool table_exists(MYSQL* mysql, const char* table_name, MYSQL_RES** result);
+
+// 从名称创建表
+int create_table(MYSQL* mysql, const char* table_name, const char* columns);
--- src/app/mysql.c
+++ src/app/mysql.c
@@ -1,64 +1,67 @@
 #include "../../include/app/mysql.h"
 
-void login_to_db(MYSQL* mysql, const char* host, const char* username, const char* password)
+int login_to_db(MYSQL* mysql, const char* host, const char* username, const char* password)
 {
-    MYSQL_RES* rslt;
-
-    if (mysql_real_connect(mysql, host, username, password, NULL, 0, NULL, 0)) {
+    if (mysql_real_connect(mysql, host, username, password, NULL, 0, NULL, 0))
+    {
         printf("MySQL服务器连接成功!\n");
-        connect_to_db(mysql, DATABASE, &rslt);
+        
+        int res;
+        res = connect_to_db(mysql, DATABASE);
+        if (res != 0)
+            return res;
+            
+        return 0;
     }
     else
     {
         printf("数据库连接错误，请检查用户名及密码配置\n");
-        return;
+        return -1;
     }
 }
 
-void connect_to_db(MYSQL* mysql, const char* db_name, MYSQL_RES** result)
+int connect_to_db(MYSQL* mysql, const char* db_name)
 {
+
     // 连接到数据库
-    bool flag = db_exists(mysql, db_name, result);
-    if (flag)
+    bool flag = db_exists(mysql, db_name);      // 查询数据库是否已存在
+    int res;                                    // 状态码
+
+    switch (flag)
     {
-        long n = (long) mysql_num_rows(*result);       // 查询结果行数
-        int res;                                    // 状态码
-        switch (n)
+    case false:                                     // 创建数据库
+        printf("数据库不存在，创建数据库...");
+        res = create_db(mysql, DATABASE);
+        if (res == 0)
+            printf("数据库创建成功，检查数据表中...\n");
+            
+        //     res = table_exists(mysql, TABLE_PROGRAM, result)
+        else
         {
-        case 0:                                     // 创建数据库
-            printf("数据库不存在，创建数据库...");
-            res = create_db(mysql, DATABASE);
-            if (res == 0)
-                printf("创建成功\n");
-            else
-            {
-                printf("数据库创建过程中出错，错误码：%d ，已退出\n", res);
-                mysql_close(mysql);
-                return;
-            }
-        default:                                    // 连接到数据库
-            res = use_db(mysql, DATABASE);           
-            if (res == 0)
-                printf("数据库连接成功\n");
-            else
-            {
-                printf("数据库连接过程中出错，错误码：%d ，已退出\n", res);
-                mysql_close(mysql);
-                return;
-            }                
-            break;
+            printf("数据库创建过程中出错，错误码：%d ，已退出\n", res);
+            mysql_close(mysql);
+            return res;
         }
-    }
-    else
-    {
-        printf("rslt is NULL!\n");
-        mysql_close(mysql);
-        return;
+    default:                                    // 连接到数据库
+        res = use_db(mysql, DATABASE);           
+        
+        if (res == 0)
+            printf("数据库连接成功\n");
+        else
+        {
+            printf("数据库连接过程中出错，错误码：%d ，已退出\n", res);
+            mysql_close(mysql);
+            return res;
+        }
+
+        return 0;
     }
 }
 
-bool db_exists(MYSQL* mysql, const char* db_name, MYSQL_RES** result)
+bool db_exists(MYSQL* mysql, const char* db_name)
 {
+    MYSQL_RES* result;
+
     char buffer[BUFFER_SIZE];
     bool flag;
 
@@ -66,13 +69,26 @@ bool db_exists(MYSQL* mysql, const char* db_name, MYSQL_RES** result)
     strcpy(buffer, "SHOW DATABASES LIKE \"" );
     strcat(buffer, db_name);
     strcat(buffer,"\"");
-    mysql_real_query(mysql, buffer, (u_long) strlen(buffer));
-    *result = mysql_store_result(mysql);
-    if (*result != NULL)
-        return true;
-    else
-        return false;
     
+    int res;
+    res = mysql_real_query(mysql, buffer, (u_long) strlen(buffer));
+    if (res != 0)
+        fprintf(stderr,"查询错误: %d\n", res);
+    result = mysql_store_result(mysql);
+    
+    if (result == NULL)
+    {
+        fprintf(stderr,"查询错误: 未返回结果\n");
+        return false;
+    }
+        
+
+    long n = (long) mysql_num_rows(result);
+    printf("n: %ld", n);
+    if (!n)
+        return false;
+    else
+        return true;    
 }
 
 
@@ -97,6 +113,38 @@ int use_db(MYSQL* mysql, const char* db_name)
 
     strcpy(buffer, "USE ");
     strcat(buffer, db_name);
+    flag = mysql_real_query(mysql, buffer, (u_long) strlen(buffer));
+
+    return flag;
+
+}
+
+bool table_exists(MYSQL* mysql, const char* table_name, MYSQL_RES** result)
+{
+    char buffer[BUFFER_SIZE];
+    bool flag;
+
+    // 查询数据库是否存在
+    strcpy(buffer, "SHOW TABLES LIKE \"" );
+    strcat(buffer, table_name);
+    strcat(buffer,"\"");
+    mysql_real_query(mysql, buffer, (u_long) strlen(buffer));
+    *result = mysql_store_result(mysql);
+    if (*result != NULL)
+        return true;
+    else
+        return false;
+    
+}
+
+int create_table(MYSQL* mysql, const char* table_name, const char* columns)
+{
+    char buffer[BUFFER_SIZE];
+    int flag;
+
+    strcpy(buffer, "CREATE TABLE ");
+    strcat(buffer, table_name);
+    strcat(buffer, columns);
     flag = mysql_real_query(mysql, buffer, (u_long) strlen(buffer));
 
     return flag;
